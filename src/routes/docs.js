8d19d0b95c781b25e11495f5839459a4ac3cee4c
Tomas <tomas.hiltunen@gmail.com>
2020-11-26 14:28:56 +0300

    master

--- src/routes/docs.js
+++ src/routes/docs.js
@@ -7,27 +7,27 @@ router.get('/', (req, res) => {
 });
 
 router.get('/doc', (req, res) => {
-  res.render('doc');
+  res.render('docs/doc');
 });
 
 router.get('/calc', (req, res) => {
-  res.render('calc');
+  res.render('docs/calc');
 });
 
 router.post('/calc', (req, res) => {
-  res.redirect('/calc');
+  res.redirect('docs/calc');
 });
 
 router.get('/nalogcalendar', (req, res) => {
-  res.render('nalogcalendar');
+  res.render('docs/nalogcalendar');
 });
 
 router.get('/workflow', (req, res) => {
-  res.render('workflow');
+  res.render('docs/workflow');
 });
 
 router.get('/risk', (req, res) => {
-  res.render('risk');
+  res.render('docs/risk');
 });
 
 module.exports = router;
