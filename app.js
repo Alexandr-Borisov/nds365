757d525ba857753132c1f2fe95f47edf1ca5aee4
Alex Borisov <arbv23@gmail.com>
2020-11-27 12:54:26 +0300

    Rel 14

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const contactRouter = require('./src/routes/contact');
 // const { checkAuth } = require('./src/middleware/auth');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 dbConnect();
 
 app.set('view engine', 'hbs');
@@ -27,12 +27,10 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cookieParser());
 
-// require('crypto').randomBytes(64).toString('hex')
-const secretSession = '46b76110a6e9e3c9cf333f02ae8fa12e46e6dad262ef5901376d34feb7145f7323cd73dbc2161ab82e167e0b60104f98b512d277649c357a9b6bcaa70259e5c8';
 app.use(
   session({
     name: 'sid',
-    secret: secretSession,
+    secret: process.env.SECRETSESSION,
     resave: false,
     store: new MongoStore({
       mongooseConnection: mongoose.connection,
