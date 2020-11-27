8a58e92b9b52ba45f41449d617fc81b4841ea8ce
Igor <shevcovigor3085@yandex.ru>
2020-11-27 10:36:09 +0300

    кнопки 60%

--- src/routes/slider.js
+++ src/routes/slider.js
@@ -7,7 +7,12 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', async (req, res) => {
-  console.log(req.body.group[0]);
-  res.render('after-slider');
+  const group = req.body.group[0];
+  const group1 = req.body.group1[0];
+  const group2 = req.body.group2[0];
+  if (group === 'yes' && group1 === 'yes' && group2 === 'yes' || group === 'no' && group1 === 'no' && group2 === 'yes') {
+    return res.render('after-slider', { status: true });
+  }
+  return res.render('after-slider');
 });
 module.exports = router;
